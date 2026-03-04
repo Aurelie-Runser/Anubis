ab6f2a4815047cbca71d910b7ee83eff3c83d328
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-03-04 21:28:33 +0100

    ajout style page 404

--- wp-content/themes/anubis-theme/resources/views/404.blade.php
+++ wp-content/themes/anubis-theme/resources/views/404.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  <h1>Erreur 404</h1>
 
   @if (! have_posts())
     <x-alert type="warning">
       {!! __('La page que vous recherchez n\'existe pas.', 'anubis-theme') !!}
     </x-alert>
 
-    <div style="margin: 10px auto">
-      <a href="{{ get_home_url() }}">Retourner à l'accueil</a>
+    <div style="margin-top: 40px;">
+      <a href="<?= home_url('/profil') ?>" class="btn">Retourner à mon profil</a>
     </div>
 
   @endif
