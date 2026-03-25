82b6ccd374d4cb01bc47f0cb7b5b12b806237643
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2026-03-25 16:26:10 +0100

    ajout guide rapport et messages

--- wp-content/themes/anubis-theme/app/dashboard-widgets.php
+++ wp-content/themes/anubis-theme/app/dashboard-widgets.php
@@ -90,14 +90,16 @@ add_action('wp_dashboard_setup', function () {
         <p>Ce site possède différents types de contenus. Voici la liste de ces derniers ainsi que leurs utilités&nbsp;:</p>
 
         <ul>
+            <li><strong>Médias&nbsp;:&nbsp;</strong>Il s'agit des différentes images utilisées sur le site pour illustrer les <strong>I.S.</strong>, les <strong>Personnages</strong>... Vous n'avez pas besoin d'y enregistrer vos images avant d'editer un contenu, vous pouvez les importer directement dans les pages d'édition des différents contenus. Vous pouvez renseigner des <strong>Légendes</strong> à vos images, cela fera du texte caché !</li>
+            <li><strong>Pages&nbsp;:&nbsp;</strong>Contenu de base utilisé seulement pour la page d'accueil et la page de profil. MERCI DE NE PAS LES SUPPRIMER SINON LE SITE SERA CASSÉ. Bien que vous puissiez en ajouter, le site n'a pas été prévu pour. Le seul moyen de rendre vos futures pages accessibles sera d'ajouter un bouton directement dans la page d'accueil.</li>
             <li><strong>Personnages&nbsp;:&nbsp;</strong>Il s'agit des différents profils visibles sur le site, ou autrement dit les membres d'Anubis. Ces derniers peuvent être reliés à un compte (les comptes sont gérés par l'administrateur) afin que les utilisateurs puissent se connecter sur le compte de ce personnage et avoir accès au même contenu que lui. Les personnages peuvent être reliés à des <strong>Dossiers</strong> pour indiquer qu'ils ont participé à telle ou telle affaire.</li>
             <li><strong>I.S.&nbsp;:&nbsp;</strong>Il s'agit des différents monstres répertoriés par Anubis. Faites-vous plaisir et mettez tout ce que vous voulez ! (tant que ce n'est pas offensant) Il serait idéal que chacun soit relié à un <strong>Dossier</strong> pour plus de cohérence, même si le <strong>Dossier</strong> en lui même est inaccessible. Même si vous n'avez pas de beau modèl 3D pour illustrer un <strong>I.S.</strong> vous pouvez utiliser un croquis, pour faire comme un "portrait robot" si l'<strong>I.S.</strong> n'a pas encore été capturé ;).</li>
             <li><strong>Dossiers&nbsp;:&nbsp;</strong>Il s'agit des différentes affaires effectuées par Anubis (par exemple le <strong>Dossier Schwach</strong>). Ces derniers regroupent beaucoup d'informations car c'est via ce contenu que des <strong>I.S.</strong> et des <strong>Personnages</strong> peuvent être liés.</li>
-            <li><strong>Médias&nbsp;:&nbsp;</strong>Il s'agit des différentes images utilisées sur le site pour illustrer les <strong>I.S.</strong>, les <strong>Personnages</strong>... Vous n'avez pas besoin d'y enregistrer vos images avant d'editer un contenu, vous pouvez les importer directement dans les pages d'édition des différents contenus. Vous pouvez renseigner des <strong>Légendes</strong> à vos images, cela fera du texte caché !</li>
-            <li><strong>Pages&nbsp;:&nbsp;</strong>Contenu de base utilisé seulement pour la page d'accueil et la page de profil. MERCI DE NE PAS LES SUPPRIMER SINON LE SITE SERA CASSÉ. Bien que vous puissiez en ajouter, le site n'a pas été prévu pour. Le seul moyen de rendre vos futures pages accessibles sera d'ajouter un bouton directement dans la page d'accueil.</li>
+            <li><strong>Rapport&nbsp;:&nbsp;</strong>Il s'agit du détail, heure par heure, voir par minutes, d'un <strong>Dossier</strong>. Les événements de l'affaire doivent y être très détaillés.</li>
+            <li><strong>Messages&nbsp;:&nbsp;</strong>Via cet onglet, vous pourrez créer des conversations entre 2 <strong>Personnages</strong>. Ces dernières seront visibles lorsque l'utilisateur est connecté à un compte relié à l'un des deux personnages. Il n'est donc pas utile de créer une conversation entre 2 <strong>Personnages</strong> si aucun d'eux n'est relié à un compte.</li>
         </ul>
 
-        <p>Mise à jour le 04/03/2026</p>
+        <p>Mise à jour le 25/03/2026</p>
 <?php
         }
     );
@@ -131,14 +133,26 @@ add_action('wp_dashboard_setup', function () {
                 </video>
             </li>
             <li>
+                <strong>Créer un Rapport pour un Dossier</strong><br>
+                <video width="100%" height="200" controls>
+                    <source src="<?php echo esc_url(home_url('?private_video=create-rapport.mp4')); ?>" type="video/mp4">
+                </video>
+            </li>
+            <li>
                 <strong>Ajouter un Historique aux Dossiers et I.S.</strong><br>
                 <video width="100%" height="200" controls>
                     <source src="<?php echo esc_url(home_url('?private_video=add-logs.mp4')); ?>" type="video/mp4">
                 </video>
             </li>
+            <li>
+                <strong>Créer des messages</strong><br>
+                <video width="100%" height="200" controls>
+                    <source src="<?php echo esc_url(home_url('?private_video=create-messages.mp4')); ?>" type="video/mp4">
+                </video>
+            </li>
         </ul>
 
-        <p>Mise à jour le 12/03/2026</p>
+        <p>Mise à jour le 25/03/2026</p>
 <?php
         }
     );
